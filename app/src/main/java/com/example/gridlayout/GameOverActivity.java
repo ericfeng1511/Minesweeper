04538675ebb79e3b6dd333c177225578e3af9680
Eric Feng <ericyfen@usc.edu>
2024-09-19 21:07:49 -0700

    added play again button

--- app/src/main/java/com/example/gridlayout/GameOverActivity.java
+++ app/src/main/java/com/example/gridlayout/GameOverActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GameOverActivity extends AppCompatActivity {
     @Override
@@ -12,7 +15,13 @@ public class GameOverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
 
-        TextView gameOverText = findViewById(R.id.gameOverText);
-        gameOverText.setText("Unlucky");
+        Button playAgainButton = findViewById(R.id.playAgainButton);
+
+        playAgainButton.setOnClickListener(view -> {
+            Intent intent = new Intent(GameOverActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        });
     }
 }
